caf6176561de7f623634bef985b4a0d2b5fa7f71
David Cortes <alexis241916@gmail.com>
2023-08-12 16:56:05 -0500

    also it is configured the urls with the modify of the files static of settings.py

--- reservas_vuelos_online_pj/reservas_vuelos_online_pj/urls.py
+++ reservas_vuelos_online_pj/reservas_vuelos_online_pj/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name= "index"), #prueba 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
